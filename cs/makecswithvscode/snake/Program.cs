72e9c1f2b5ff4329319d9c2c2243760315719618
sylwan bouderbala <sylwanbouderbala@outlook.fr>
2019-09-14 12:29:08 +0200

    ajout snake

---
+++ cs/makecswithvscode/snake/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace snake
+{
+    class Program
+    {
+        static int [,] creationmatrice()
+        {
+            int[,] mat = [20, 20];
+            for (int i =0; i<mat.GetLength(1); )
+
+            return mat;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
