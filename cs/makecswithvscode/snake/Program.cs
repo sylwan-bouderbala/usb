8a1226456079305d73db5545f8df9a1a96c6ff94
sylwan bouderbala <sylwanbouderbala@outlook.fr>
2019-09-15 18:22:37 +0200

    travail sur snake

--- cs/makecswithvscode/snake/Program.cs
+++ cs/makecswithvscode/snake/Program.cs
@@ -4,16 +4,36 @@ namespace snake
 {
     class Program
     {
+        static int longfurtr
+        static void affichercarte (int[,] carte)
+        {
+            for(int i=0; i<carte.GetLength(1); i++)
+            {
+                for (int j=0; j<carte.GetLength(0); j++)
+                {
+                    Console.Write(carte[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
         static int [,] creationmatrice()
         {
-            int[,] mat = [20, 20];
-            for (int i =0; i<mat.GetLength(1); )
+            int[,] mat = new int[15, 15];
+            for (int i =0; i<mat.GetLength(1); i++)
+            {
+                for (int j = 0; j < mat.GetLength(0); j++)
+                {
+                    mat[i, j] = 0;
+                }
+
+            }
 
             return mat;
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] carte = creationmatrice();
+            affichercarte(carte);
         }
     }
 }
